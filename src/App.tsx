403ad792718e09f13b9e4c13279d7275ad014110
demongodYY <qi.yu@thoughtworks.com>
2023-04-06 02:23:32 +0800

    fix: some style and root suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import './App.css';
 import Hello from './components/Hello';
 import HelloName from './components/HelloName';
@@ -5,8 +6,10 @@ import HelloName from './components/HelloName';
 function App() {
   return (
     <div className="App">
-      <HelloName />
-      <Hello />
+      <Suspense fallback={<h2>Loading……</h2>}>
+        <HelloName />
+        <Hello />
+      </Suspense>
     </div>
   );
 }
--- src/components/HelloName.tsx
+++ src/components/HelloName.tsx
@@ -6,7 +6,11 @@ export default () => {
     <div>
       <label>
         请输入名字：
-        <Input value={helloName} onChange={onHelloNameChange}></Input>
+        <Input
+          value={helloName}
+          onChange={onHelloNameChange}
+          style={{ width: '320px' }}
+        ></Input>
       </label>
     </div>
   );
