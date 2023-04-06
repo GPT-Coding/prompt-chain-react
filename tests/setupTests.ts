cc68026ae8f171af6e0f6a9dd55fc2e1b823e9c8
demongodYY <qi.yu@thoughtworks.com>
2023-04-06 22:35:38 +0800

    fix: mock matchMedia

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -2,6 +2,21 @@ import '@testing-library/jest-dom';
 import { server } from '../mocks/node';
 import 'whatwg-fetch';
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+  server.listen();
+});
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
