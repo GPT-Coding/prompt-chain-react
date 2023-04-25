3ae5116afc637ea5183e48f813a13937e5be4514
demongodYY <qi.yu@thoughtworks.com>
2023-04-25 11:09:23 +0800

    add router for page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Hello from './components/Hello';
 import HelloName from './components/HelloName';
@@ -8,9 +9,20 @@ function App() {
   return (
     <div className="App">
       <Suspense fallback={<h2>Loading……</h2>}>
-        <HelloName />
-        <Hello />
-        <MerchandiseList />
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <HelloName />
+                  <Hello />
+                </>
+              }
+            ></Route>
+            <Route path="products" element={<MerchandiseList />}></Route>
+          </Routes>
+        </Router>
       </Suspense>
     </div>
   );
